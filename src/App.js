6b867ea0d511eef715fa35cb7d986b467c7c8f02
rezalatihan <rezacatria1@gmail.com>
2022-09-14 11:08:38 -0700

    coba lagi mengganti tulasan p menjadi reza

--- src/App.js
+++ src/App.js
@@ -6,7 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Reza Catria Akbar</p>
+        <p>
+          Reza catria akbar
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
