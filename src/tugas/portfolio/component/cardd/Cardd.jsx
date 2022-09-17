0d59123ba8ac4a9ee020bcf9b05de8aa877923d1
rezalatihan <rezacatria1@gmail.com>
2022-09-17 11:20:19 -0700

    membuat portfolio

--- src/tugas/portfolio/component/cardd/Cardd.jsx
+++ src/tugas/portfolio/component/cardd/Cardd.jsx
@@ -11,37 +11,42 @@ function Cardd() {
           <Card.Img variant="top" src="css.png" />
           <Card.Body>
             <Card.Title>css</Card.Title>
-            <Card.Text>This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</Card.Text>
+            <Card.Text>
+              CSS adalah singkatan dari cascading style sheets, yaitu bahasa yang digunakan untuk menentukan tampilan dan format halaman website. Dengan CSS, Anda bisa mengatur jenis font, warna tulisan, dan latar belakang halaman.
+            </Card.Text>
           </Card.Body>
         </Card>
       </Col>
-
-      <Col>
-        <Card>
-          <Card.Img variant="top" src="git.png" />
-          <Card.Body>
-            <Card.Title>Git</Card.Title>
-            <Card.Text>This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
-
       <Col>
         <Card>
           <Card.Img variant="top" src="klipartz.com.png" />
           <Card.Body>
             <Card.Title>HTML</Card.Title>
-            <Card.Text>This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</Card.Text>
+            <Card.Text>
+              HTML adalah bahasa yang digunakan untuk membuat halaman web. Bagi yang berkecimpung di dunia perangkat gadget dan ilmu komputer, pasti sudah tidak asing lagi dengan berbagai kode di dalamnya. HTML adalah kependekan dari
+              Hypertext Markup Language.
+            </Card.Text>
           </Card.Body>
         </Card>
       </Col>
-
+      <Col>
+        <Card>
+          <Card.Img variant="top" src="git.png" />
+          <Card.Body>
+            <Card.Title>Git</Card.Title>
+            <Card.Text>
+              gitHub adalah platform khusus developer yang dibuat karena terinspirasi dari cara bekerja para programmer. Dari open source ke bisnis, kita bisa meng-host dan meninjau sebuah kode, mengelola project, dan membangun perangkat
+              lunak bersama 56 juta lebih pengguna di seluruh dunia.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
       <Col>
         <Card>
           <Card.Img variant="top" src="React.png" />
           <Card.Body>
             <Card.Title>React</Card.Title>
-            <Card.Text>This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</Card.Text>
+            <Card.Text>React JS adalah library JavaScript yang biasa digunakan saat membangun UI suatu website atau aplikasi web.</Card.Text>
           </Card.Body>
         </Card>
       </Col>
